a1a9229c99b551e08267c3fa1c558da3fdfbe6b3
rakuraku.musen <musen.rakuraku@gmail.com>
2018-08-21 03:44:40 +0000

    taskapp model

---
+++ app/models/taskapp.rb
@@ -0,0 +1,2 @@
+class Taskapp < ApplicationRecord
+end
---
+++ db/migrate/20180821020152_create_taskapps.rb
@@ -0,0 +1,9 @@
+class CreateTaskapps < ActiveRecord::Migration[5.0]
+  def change
+    create_table :taskapps do |t|
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
